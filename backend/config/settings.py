6306819d70699714871ab321ce53e53791e49483
mszahan <mszahan1@gmail.com>
2022-09-20 13:54:43 +0600

    added project level permission

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,3 +137,12 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+    ]
+}
