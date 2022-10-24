21c96fc2d3888d18f467ab22a928d0983e5fe970
mszahan <mszahan1@gmail.com>
2022-10-24 18:38:17 +0600

    added schema to the api and installed the pymll and uritamplate

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,13 +12,18 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('api/', include('blog_api.urls')),
     path('api/user/', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('blog.urls')),
+    path('schema', get_schema_view(
+        title="BlogApi",
+        description="API for the blog site",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
 
 
